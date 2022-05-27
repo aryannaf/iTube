7b6bbd969c3757273f52b14c5bea2424fb3adb54
aryannaf <aryannafarhadi@gmail.com>
2022-05-26 23:30:59 -0400

    Added header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,27 +6,29 @@ import avatar from '../../assets/images/Mohan-muruge.jpg';
 
 
 export default function Header() {
-    
+
     return (
         <header className="header">
             <img className="header__logo"
                 src={logo}
                 alt="BrainFlix Logo"
             />
-            <input className="header__search"
-                type="text"
-                placeholder="Search"
-            />
+            <div className="header__feature">
+                <input className="header__search"
+                    type="text"
+                    placeholder="Search"
+                />
+                <img className="header__avatar"
+                    src={avatar}
+                    alt="profile picture"
+                />
+            </div>
             <button className="header__upload">
                 <img className="header__upload__icon"
                     src={uploadIcon}
                     alt="upload icon"
                 />
                 UPLOAD</button>
-            <img className="header__avatar"
-                src={avatar}
-                alt="profile picture"
-            />
         </header>
     )
 }
--- src/components/VideoInfo/VideoInfo.js
+++ src/components/VideoInfo/VideoInfo.js
@@ -1,10 +1,58 @@
-import Display from '../Display/Display';
 import './VideoInfo.scss';
+import '../../data/video-details.json';
+import views from '../../assets/images/views.svg';
+import likes from '../../assets/images/likes.svg';
+import avatar from '../../assets/images/Mohan-muruge.jpg';
+import commentIcon from '../../assets/images/add_comment.svg';
 
 export default function VideoInfo() {
+
+
     return (
-        <section className="">
-            
+        <section className="video">
+            <img className="video__display"
+                src=""
+                alt=""
+            />
+            <h1></h1>
+            <div>
+                <div>
+                    <h6></h6>
+                    <p></p>
+                </div>
+                <div>
+                    <div>
+                        <img className="video__display__"
+                            src={views}
+                            alt="views icon"
+                        />
+                        <p></p>
+                    </div>
+                    <div>
+                        <img className="video__display__"
+                            src={likes}
+                            alt="likes icon"
+                        />
+                    </div>
+                </div>
+            </div>
+            <p></p>
+            <h6></h6>
+            <div>
+                <img className="video__comment__avatar" src={avatar} alt="profile picture"/>
+                <div>
+                    <h3>JOIN THE CONVERSATION</h3>
+                    <textarea className="" placeholder="Add a new comment"
+                    >
+                    </textarea>
+                    <button>
+                        <img className=""
+                            src={commentIcon}
+                            alt="comment icon"
+                        />
+                        COMMENT</button>
+                </div>
+            </div>
         </section>
     )
 }

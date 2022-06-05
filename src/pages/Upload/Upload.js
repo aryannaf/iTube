9ae7b7cef96d7a00c73d064981144caa3ab4e72e
aryannaf <aryannafarhadi@gmail.com>
2022-06-05 18:56:49 -0400

    Updated upload page styling with tablet and desktop media queries

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -7,25 +7,39 @@ import thumbnail from '../../assets/images/Upload-video-preview.jpg';
 
 function Upload() {
 
+    const uploadSuccess = () => {
+        alert("Upload Successful!");
+    }
+
     return (
         <main className="upload">
             <div className="upload__section">
                 <h1 className="upload__title">Upload Video</h1>
                 <form className="upload__form">
-                    <h4 className="upload__thumbnail-title">VIDEO THUMBNAIL</h4>
-                    <img className="upload__thumbnail-image" src={thumbnail} alt="video thumbnail" />
-                    <h4 className="upload__title-prompt">TITLE YOUR VIDEO</h4>
-                    <input className="upload__title-input" type="text" placeholder="Add a title to your video"></input>
-                    <h4 className="upload__description-title">ADD A VIDEO DESCRIPTION</h4>
-                    <textarea className="upload__description-input" placeholder="Add a description to your video"></textarea>
-                    <NavLink to="/" className="upload__publish-link">
-                        <button className="upload__publish" type="submit">
-                            <img className="upload__publish-icon" src={publish} alt="publish icon" />
-                            PUBLISH
-                        </button>
-                    </NavLink>
+                    <div className="upload__inputs">
+                        <div className="upload__thumbnail">
+                            <h4 className="upload__thumbnail-title">VIDEO THUMBNAIL</h4>
+                            <img className="upload__thumbnail-image" src={thumbnail} alt="video thumbnail" />
+                        </div>
+                        <div className="upload__form-fields">
+                            <h4 className="upload__title-prompt">TITLE YOUR VIDEO</h4>
+                            <input className="upload__title-input" type="text" name="title" placeholder="Add a title to your video" required></input>
+                            <h4 className="upload__description-title">ADD A VIDEO DESCRIPTION</h4>
+                            <textarea className="upload__description-input" name="description" placeholder="Add a description to your video" required></textarea>
+                        </div>
+                    </div>
+                    <div className="upload__buttons">
+                        <NavLink to="/" className="upload__publish-link" onClick={() => { uploadSuccess() }} >
+                            <button className="upload__publish" type="submit">
+                                <img className="upload__publish-icon" src={publish} alt="publish icon" />
+                                PUBLISH
+                            </button>
+                        </NavLink>
+                        <NavLink to="/" className="upload__cancel-link">
+                            <button className="upload__cancel">CANCEL</button>
+                        </NavLink>
+                    </div>
                 </form>
-                <button className="upload__cancel">CANCEL</button>
             </div>
         </main>
     )
